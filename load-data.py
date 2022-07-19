6f5cec2edeedbd082ac1f64f0bd07a7a8ef89e26
antonum <aumnikov@gmail.com>
2022-07-19 19:06:55 -0400

    initial commit

---
+++ load-data.py
@@ -0,0 +1,19 @@
+import redis
+import json
+import os
+r = redis.Redis(host='localhost', port=6379, db=0)
+#d = {"hello": "world", "some": "value"}
+#r.json().set("somekey", "$", d)
+#print(r.json().get("somekey"))
+
+filenames = os.listdir('data')
+for filename in filenames:
+    print('processing file '+filename)
+    f = open('data/'+filename)
+    bundle=json.load(f)
+    for entry in bundle['entry']:
+        resource_type=entry['resource']['resourceType']
+        resource_id=entry['resource']['id']
+        key=resource_type+":"+resource_id
+        body=entry['resource']
+        r.json().set(key, "$", body)
